ec5ac629d5b17665e061d9c957d8fc71404765dc
Amr Hashem <amr.m.hashem@gmail.com>
2020-08-28 15:56:12 +0200

    models change

--- src/job/models.py
+++ src/job/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+'''
+ django model field : 
+    - html widget
+    - validation 
+    - db size 
+'''
+
+JOB_TYPE = (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+
+def image_upload(instance,filename):
+    imagename , extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+
+
+
+class Job(models.Model):  # table 
+    title = models.CharField(max_length=100)  # column
